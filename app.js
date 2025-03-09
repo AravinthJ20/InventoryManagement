8b07665201dcc2a4d70cf628679091ba84c38d6a
AravintJ20 <aravinthj388@gmail.com>
2025-03-09 11:05:51 +0530

    merge conflict test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 require('dotenv').config()
 const app=express()
+let test=2;
 const PORT=process.env.PORT
 const path=require('path')
 const {connectDB}=require('./config/db')
